c6a5bff2d59d4b3950bf216d01b627131ff5ca4b
nikaraa <giuseppemalara00001@gmail.com>
2023-02-26 23:30:43 +0100

    Exercise 2 done.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let res=string;
+    for(let i=0;i<num-1;i++){
+        res+=string;
+    }
+    return res;
 };
 
 // Do not edit below this line
