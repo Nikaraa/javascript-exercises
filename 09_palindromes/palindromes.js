452dd2f24b4b6752ff00c1d981aa3fd87735b5cd
nikaraa <giuseppemalara00001@gmail.com>
2023-03-21 20:34:22 +0100

    Did palindromes ex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const isPalindromes=string.toLowerCase().replace(/[^a-z]/g, "");
+    if(isPalindromes==isPalindromes.split("").reverse().join("")){
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
