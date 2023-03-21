37edf24eded9fc2da083677821632de390ba13d5
nikaraa <giuseppemalara00001@gmail.com>
2023-03-21 21:25:14 +0100

    Did exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let array= books.map(book=> 
+        book.title);
+    return array
 };
 
 // Do not edit below this line
