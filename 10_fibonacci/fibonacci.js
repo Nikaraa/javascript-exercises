884180220536ae36147b0d5533fcb8d50ff529d5
nikaraa <giuseppemalara00001@gmail.com>
2023-03-21 20:59:02 +0100

    Did fibonacci ex

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if (number < 0) return "OOPS"
+    //every number is the sum of the precedent twos so
+    let first = 0;
+    let second = 1;
+    let sum=0;
+    for (let i = 1; i < number; i++) {
+        sum = first + second;
+        first = second;
+        second = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
