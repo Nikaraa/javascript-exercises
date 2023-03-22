7bdaa3d57d43c2c0d9aae202dc1b9e1bf8bc4ef9
nikaraa <giuseppemalara00001@gmail.com>
2023-03-22 17:46:13 +0100

    Did exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    const res= array.reduce((oldest, current) => {
+        const oldAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+        return oldAge < currentAge ? current : oldest;
+    })
+    return res;
 };
+
+function getAge(birthDate, deathDate) {
+    if (!deathDate) {
+        deathDate = new Date().getFullYear()
+    }
+    return deathDate - birthDate
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
